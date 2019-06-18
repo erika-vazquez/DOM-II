c49631e17dfdeb58efeb0c24d8049db9a8e0d5c6
Erika <ito2016@protonmail.ch>
2019-06-18 15:18:22 -0500

    Added a dblclick event listener to the Welcome! heading, to display different text when double clicked

--- js/index.js
+++ js/index.js
@@ -20,13 +20,21 @@ button.addEventListener("click", function () {
     alert("You've clicked me!");
 });
 
-//When hover over the logo, the text color changes to red for 3 seconds
+//using mouseover event listener where when hover over the logo, the 
+//text color changes to red for 3 seconds
 let logo = document.querySelector(".logo-heading");
 
-logoHeading.addEventListener("mouseover", () => {
+logo.addEventListener("mouseover", () => {
     document.body.style.color = "red";
-    
+    //setTimeout to reset color after 3sec
     setTimeout(function() {
         document.body.style.color = "";
       }, 3000);//3 seconds
     }, false );
+
+//
+let welcomeH2 = document.querySelector(".intro h2");
+
+welcomeH2.addEventListener("dblclick", () => {
+    welcomeH2.innerHTML = "Surprise!";
+});    

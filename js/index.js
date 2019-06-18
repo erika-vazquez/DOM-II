fe5b0ec08815b01d00a1a5224569e54d9404bbdf
Erika <ito2016@protonmail.ch>
2019-06-18 13:40:16 -0500

    Added click event listener on the first sign me up button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+//create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
+/*
+mouseover
+keydown
+wheel
+drag / drop
+load
+focus
+resize
+scroll
+select
+dblclick
+*/
+//Using the click event listener on the first Sign Me Up! button.
+//When clicked, an alert pops up with a message.
+    const button = document.querySelector('.btn');
+
+    button.addEventListener("click", function(){
+    alert("You've clicked me!");
+});
